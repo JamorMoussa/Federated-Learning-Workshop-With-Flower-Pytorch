e61348f06be68440b3489a634c8c58569f02310e
jamormoussa <moussajamorsup@gmail.com>
2024-10-05 15:16:07 +0100

    add setup file for this project

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lilytorch',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    # Metadata
+    author='',
+    author_email='',
+    description='',
+    url=''
+)
